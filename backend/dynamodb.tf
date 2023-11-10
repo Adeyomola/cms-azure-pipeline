2758ec1913e35305fd7a205be5a8ea6f3fc87da7
Adeyomola <kayzeel@yahoo.com>
2023-11-11 00:09:32 +0100

    added role for DynamoDB

--- backend/dynamodb.tf
+++ backend/dynamodb.tf
@@ -8,3 +8,25 @@ resource "aws_dynamodb_table" "state_locking" {
     type = "S"
   }
 }
+
+data "aws_iam_policy_document" "ddb_policy" {
+  statement {
+    actions = [
+        "dynamodb:DescribeTable",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem"
+      ],
+    Effect = "Allow",
+    Resource = "arn:aws:dynamodb:*:*:table/${var.table_name}",
+    principals {
+      type        = "Service"
+      identifiers = [aws_dynamodb_table.state_locking.id]
+    }
+  }
+}
+
+resource "aws_iam_role" "ddb_role" {
+  name        = "Dynamodb_Role"
+  assume_role_policy = data.aws_iam_policy_document.ddb_policy.json
+}
